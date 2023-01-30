1834b031611cc1358b45970af0fb7b4cc134063e
msj9965 <121001545+msj9965@users.noreply.github.com>
2023-01-30 23:19:21 +0900

    [Silver III] Title: N과 M (5), Time: 352 ms, Memory: 28260 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,67 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static int[]arr;
+    public static boolean[] value;
+    public static int n,m;
+    public static int[]list;
+
+    public static StringBuilder sb= new StringBuilder();
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st=new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr= new int[n];
+        value=new boolean[n];
+        list=new int[n];
+        st= new StringTokenizer(br.readLine());
+        for(int i =0;i<n;i++){
+            list[i]=Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(list);
+        dfs(0);
+
+
+        bw.write(String.valueOf(sb));
+        bw.close();
+
+    }
+
+
+
+
+
+
+    public static void dfs(int d) {
+        if(d==m){
+            for(int i=0;i<m;i++){
+                sb.append(arr[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+
+        }
+        for(int i=0;i<n;i++){
+            if(!value[i]){
+                value[i]=true;
+                arr[d]=list[i];
+                dfs(d+1);
+                value[i]=false;
+            }
+        }
+    }
+
+
+
+
+}
+
+
+
+
