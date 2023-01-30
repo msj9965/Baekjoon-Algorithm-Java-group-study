54c2dea787d415992c99904417d90c3be03fadf9
msj9965 <121001545+msj9965@users.noreply.github.com>
2023-01-31 00:26:01 +0900

    [Bronze II] Title: 너의 이름은 몇 점이니?, Time: 116 ms, Memory: 14176 KB -BaekjoonHub

---
+++ 백준/Bronze/15813. 너의 이름은 몇 점이니？/너의 이름은 몇 점이니？.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n= Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder(br.readLine());
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum= sum+(int)sb.charAt(i)-64;
+
+        }
+        bw.write(String.valueOf(sum));
+        bw.close();
+    }
+}
