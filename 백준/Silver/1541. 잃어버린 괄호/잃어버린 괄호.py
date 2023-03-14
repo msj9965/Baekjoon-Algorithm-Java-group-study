6d1f8c30e6b2c48aaf3e5082122dd34d7d48e9bf
msj9965 <121001545+msj9965@users.noreply.github.com>
2023-03-14 19:43:11 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -1,16 +1,9 @@
-import re
-s = input()
-if s.find('-') != -1:
-    s1 = s[0:s.find('-')]
-    s2 = s[s.find('-')+1:len(s)]
-    
-    s1_num = list(re.split(r'-|\+', s1))
-    s2_num = list(re.split(r'-|\+', s2))
-    s1_num = list(map(int,s1_num))
-    s2_num = list(map(int, s2_num))
-    result = sum(s1_num)-sum(s2_num)
-else :
-    s_num = list(re.split(r'-|\+',s))
-    s_num = list(map(int, s_num))
-    result = sum(s_num)
+s = input().split('-')
+result =0 
+for i in s[0].split('+') :
+    result += int(i)
+for i in s[1:]:
+    for k in i.split('+'):
+        result-=int(k)
+        
 print(result)
