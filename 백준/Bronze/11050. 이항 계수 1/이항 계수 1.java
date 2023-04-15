3ca10b4d9aab6df43afc83b8ed66a67187a0ac36
msj9965 <121001545+msj9965@users.noreply.github.com>
2023-04-15 16:13:00 +0900

    [Bronze I] Title: 이항 계수 1, Time: 120 ms, Memory: 14292 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[]ar)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int [][]arr = new int[n+1][k+1];
+        for(int i = 0;i<=n;i++){
+            for(int j = 0;j<=Math.min(i,k);j++){
+                if(i==0 || j==i||j==0){
+                    arr[i][j]=1;
+                }
+                else {
+                    arr[i][j]=arr[i-1][j-1]+arr[i-1][j];
+                }
+            }
+        }
+        bw.write(String.valueOf(arr[n][k]));
+        bw.close();
+
+
+    }
+}
